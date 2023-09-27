bc9c2ed2d64a594e6256be22c4c7a3bbb15683bc
El hadji Rawane Meissa Sow <96692209+RawaneG@users.noreply.github.com>
2023-09-27 18:39:59 +0000

    Update

--- components/nav/Heading.js
+++ components/nav/Heading.js
@@ -6,7 +6,7 @@ export const Heading = () => {
   return (
     <header className={styles.heading}>
       <MyLinks />
-      <OutlineButton onClick={() => window.open("/fake_resume.pdf")}>
+      <OutlineButton onClick={() => window.open("/Web_Developer.pdf")}>
         My resume
       </OutlineButton>
     </header>
