aa7d47da151f6e4057cbafa324bcddaea739f2eb
El hadji Rawane Meissa Sow <96692209+RawaneG@users.noreply.github.com>
2023-09-27 02:50:52 +0000

    first commit

---
+++ components/home/about/Stats.js
@@ -0,0 +1,55 @@
+import styles from "./stats.module.scss";
+import { Reveal } from "@/components/utils/Reveal";
+import { AiFillCode, AiFillSmile } from "react-icons/ai";
+
+export const Stats = () => {
+  return (
+    <div className={styles.stats}>
+      <Reveal>
+        <div className={styles.statColumn}>
+          <h4>
+            <AiFillCode size="2.4rem" color="var(--brand)" />
+            <span>Use at work</span>
+          </h4>
+          <div className={styles.statGrid}>
+            <span className="chip">PHP</span>
+            <span className="chip">SQL</span>
+            <span className="chip">HTML</span>
+            <span className="chip">Jira</span>
+            <span className="chip">React</span>
+            <span className="chip">NodeJS</span>
+            <span className="chip">GitHub</span>
+            <span className="chip">Heroku</span>
+            <span className="chip">Angular</span>
+            <span className="chip">Symfony</span>
+            <span className="chip">Laravel</span>
+            <span className="chip">Express</span>
+            <span className="chip">MongoDB</span>
+            <span className="chip">REST APIs</span>
+            <span className="chip">BitBucket</span>
+            <span className="chip">CSS / SASS</span>
+            <span className="chip">JavaScript</span>
+            <span className="chip">TypeScript</span>
+          </div>
+        </div>
+      </Reveal>
+      <Reveal>
+        <div className={styles.statColumn}>
+          <h4>
+            <AiFillSmile size="2.4rem" color="var(--brand)" />
+            <span>Use for fun</span>
+          </h4>
+          <div className={styles.statGrid}>
+            <span className="chip">Java</span>
+            <span className="chip">Figma</span>
+            <span className="chip">Python</span>
+            <span className="chip">Tailwind</span>
+            <span className="chip">CommerceJS</span>
+            <span className="chip">Spring Boot</span>
+            <span className="chip">Framer Motion</span>
+          </div>
+        </div>
+      </Reveal>
+    </div>
+  );
+};
---
+++ components/home/contact/Contact.js
@@ -0,0 +1,47 @@
+import { Reveal } from "@/components/utils/Reveal";
+import styles from "./contact.module.scss";
+import { AiFillMail } from "react-icons/ai";
+import Link from "next/link";
+
+export const Contact = () => {
+  return (
+    <section className="section-wrapper" id="contact">
+      <div className={styles.contactWrapper}>
+        <Reveal width="100%">
+          <h4 className={styles.contactTitle}>
+            Contact<span>.</span>
+          </h4>
+        </Reveal>
+        <Reveal width="100%">
+          <p className={styles.contactCopy}>
+            Shoot me an email if you want to connect! You can also find me on{" "}
+            <Link
+              href="https://www.linkedin.com/in/el-hadji-rawane-meissa-sow-b870051a5/"
+              target="_blank"
+              rel="nofollow"
+            >
+              Linkedin
+            </Link>{" "}
+            or{" "}
+            <Link
+              href="https://twitter.com/hadji_meissa"
+              target="_blank"
+              rel="nofollow"
+            >
+              Twitter
+            </Link>{" "}
+            if that&apos;s more your speed.
+          </p>
+        </Reveal>
+        <Reveal width="100%">
+          <Link href="mailto:rawane.meissa@gmail.com">
+            <div className={styles.contactEmail}>
+              <AiFillMail size="2.4rem" />
+              <span>rawane.meissa@gmail.com</span>
+            </div>
+          </Link>
+        </Reveal>
+      </div>
+    </section>
+  );
+};
